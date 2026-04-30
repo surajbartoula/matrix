dbc90b490e7e79462d3b983edbece83c24fd0b29
suraj bartoula <suraj_bartoula@yahoo.com>
2026-04-30 09:59:11 +0200

    adding comment

--- math_lib/matrix.py
+++ math_lib/matrix.py
@@ -99,6 +99,7 @@ class Matrix:
                 break
             # Step 1: Find the best row for this pivot (Partial Pivoting)
             sel_row = pivot_row
+            # Finiding non-zero pivot
             while sel_row < self.rows and abs(rows_list[sel_row][pivot_col]) < 1e-9:
                 sel_row += 1
             if sel_row == self.rows: # No pivot in this column
