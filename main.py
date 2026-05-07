ec981ac3c1a28e3d1931c66a8297fdf7f7e492b2
suraj bartoula <suraj_bartoula@yahoo.com>
2026-05-07 22:12:24 +0200

    fixing main

--- main.py
+++ main.py
@@ -1,6 +1,107 @@
 from math_lib import Matrix, Vector, cross_product, angle_cos, linear_combination, lerp
 
 
+def vector_operations():
+    print("--- Vector Addition ---")
+    u = Vector([2., 3.])
+    v = Vector([5., 7.])
+    u.add(v)
+    print(u)
+    print("--- Vector Substraction ---")
+    u = Vector([2., 3.])
+    v = Vector([5., 7.])
+    u.sub(v)
+    print(u)
+    print("--- Vector scaling ---")
+    u = Vector([2., 3.])
+    u.scl(2)
+    print(u)
+    print("--- Matrix Opertions ---")
+    u = Matrix([
+        [1., 2.],
+        [3., 4.]
+    ])
+    v = Matrix([
+        [7., 4.],
+        [-2., 2.]
+    ])
+    u.add(v)
+    print(u)
+    u = Matrix([
+        [1., 2.],
+        [3., 4.]
+    ])
+    v = Matrix([
+        [7., 4.],
+        [-2, 2.]
+    ])
+    u.sub(v)
+    print(u)
+    u = Matrix([
+        [1., 2.],
+        [3., 4.]
+    ])
+    u.scl(2.)
+    print(u)
+
+def test_linear_combination():
+    print("--- Linear Combination ---")
+    e1 = Vector([1., 0., 0.])
+    e2 = Vector([0., 1., 0.])
+    e3 = Vector([0., 0., 1.])
+    v1 = Vector([1., 2., 3.])
+    v2 = Vector([0., 10., -100.])
+    print(linear_combination([e1, e2, e3], [10., -2., 0.5]))
+    print(linear_combination([v1, v2], [10., -2.]))
+
+def test_lerp():
+    print("--- Lerp ---")
+    print(lerp(0., 1., 0.))
+    print(lerp(0., 1., 1.))
+    print(lerp(0., 1., 0.5))
+    print(lerp(21., 42., 0.3))
+    print(lerp(Vector([2., 1.]), Vector([4., 2.]), 0.3))
+    print(lerp(Matrix([[2., 1.], [3., 4.]]), Matrix([[20., 10.], [30., 40.]]), 0.5))
+
+def test_dot_product():
+    print("--- Dot Product ---")
+    u = Vector([0., 0.])
+    v = Vector([1., 1.])
+    print(u.dot(v))
+    u = Vector([1., 1.])
+    v = Vector([1., 1.])
+    print(u.dot(v))
+    u = Vector([-1., 6.])
+    v = Vector([3., 2.])
+    print(u.dot(v))
+
+def test_norm():
+    print("--- Norm ---")
+    u = Vector([0., 0., 0.])
+    print(u.norm_1(), u.norm(), u.norm_inf())
+    u = Vector([1., 2., 3.])
+    print(u.norm_1(), u.norm(), u.norm_inf())
+    u = Vector([-1., -2.])
+    print(u.norm_1(), u.norm(), u.norm_inf())
+
+def test_cosine():
+    print("--- Cosine ---")
+    u = Vector([1., 0.])
+    v = Vector([1., 0.])
+    print(angle_cos(u, v))
+    u = Vector([1., 0.])
+    v = Vector([0., 1.])
+    print(angle_cos(u, v))
+    u = Vector([-1., 1.])
+    v = Vector([1., -1.])
+    print(angle_cos(u, v))
+    u = Vector([2., 1.])
+    v = Vector([4., 2.])
+    print(angle_cos(u, v))
+    u = Vector([1., 2., 3.])
+    v = Vector([4., 5., 6.])
+    print(angle_cos(u, v))
+
 def test_cross_product():
     print("--- Exercise 06: Cross Product ---")
     u1, v1 = Vector([0., 0., 1.]), Vector([1., 0., 0.])
@@ -11,6 +112,54 @@ def test_cross_product():
 
     u3, v3 = Vector([4., 2., -3.]), Vector([-2., -5., 16.])
     print(f"u3 X v3 = {cross_product(u3, v3)}")
+
+def test_matrix_multipication():
+    print("--- Matrix Multiplication ---")
+    u = Matrix([
+        [1., 0.],
+        [0., 1.]
+    ])
+    v = Vector([4., 2.])
+    print(u.mul_vec(v))
+    u = Matrix([
+        [2., 0.],
+        [0., 2.],
+    ])
+    v = Vector([4., 2.])
+    print(u.mul_vec(v))
+    u = Matrix([
+        [2., -2],
+        [-2., 2]
+    ])
+    v = Vector([4., 2.])
+    print(u.mul_vec(v))
+    u = Matrix([
+        [1., 0.],
+        [0., 1.]
+    ])
+    v = Matrix([
+        [1., 0.],
+        [0., 1.]
+    ])
+    print(u.mul_mat(v))
+    u = Matrix([
+        [1., 0.],
+        [0., 1.]
+    ])
+    v = Matrix([
+        [2., 1.],
+        [4., 2.]
+    ])
+    print(u.mul_mat(v))
+    u = Matrix([
+        [3., -5.],
+        [6., 8.]
+    ])
+    v = Matrix([
+        [2., 1.],
+        [4., 2.]
+    ])
+    print(u.mul_mat(v))
 
 def test_trace():
     print("--- Exercise 08: Trace ---")
@@ -154,24 +303,15 @@ def test_rank():
     ])
     print(u3.rank())
 
-# def main():
-#     print("---Vector Addition---")
-#     u = Vector([2., 3.])
-#     v = Vector([5., 7.])
-#     u.add(v)
-#     print(u)
-
-#     print("\n---Matrix Scaling---")
-#     u_mat = Matrix([
-#         [1., 3.],
-#         [2., 4.]
-#     ])
-#     u_mat.scl(2.)
-#     print(u_mat)
-
-
 if __name__ == "__main__":
+    vector_operations()
+    test_linear_combination()
+    test_lerp()
+    test_dot_product()
+    test_norm()
+    test_cosine()
     test_cross_product()
+    test_matrix_multipication()
     test_trace()
     test_transpose()
     test_rref()
